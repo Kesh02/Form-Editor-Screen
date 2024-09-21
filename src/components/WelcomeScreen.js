0cbc7d810f498c19f172f6d27de9a1b435974b3f
it22317780 <gimhanii2002@gmail.com>
2024-09-21 16:00:02 +0530

    update styles

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import './styles.css'; 
-
-// const WelcomeScreen = ({ title, description }) => {
-//   return (
-    
-//     <div className="welcome-container">
-      
-//       <h1 className="welcome-title">{title}</h1>
-//       <p className="welcome-description">{description}</p>
-//       <button className="start-button">Start</button>
-//     </div>
-//   );
-// };
-
-// export default WelcomeScreen;
-
 import React from 'react';
 
 function WelcomeScreen({ title, description, image }) {
@@ -22,7 +5,7 @@ function WelcomeScreen({ title, description, image }) {
     <div className="welcome-screen">
       <h1>{title}</h1>
       <p>{description}</p>
-      {/* Display the image if it exists */}
+      {/* Display the image */}
       {image && (
         <img src={URL.createObjectURL(image)} alt="Uploaded" className="welcome-image" />
       )}
